2629a8f3366fd5860b7d06825a5b36b291807fdc
Leigh Varley <leighvarley@gmail.com>
2015-06-27 17:41:32 -0400

    Updated getWinner function
    Used if .. else conditional statements to determine whether the player or the computer won.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -49,6 +49,21 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+      if (playerMove === computerMove){
+        winner = 'tie';
+    }
+    else if (playerMove === 'rock' && computerMove === 'sissors'){
+        winner = 'player';
+    }
+    else if (playerMove === 'sissors' && computerMove === 'paper'){
+        winner = 'player';
+    }
+    else if (playerMove === 'paper' && computerMove === 'rock'){
+        winner = 'player';
+    }
+    else {
+        winner = 'computer';
+    }
     return winner;
 }
 
